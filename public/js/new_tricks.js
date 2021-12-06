789d80e9aaefa7de051bfb9e0a4032a4fef8bcaf
Marie <Marue59@hotmail.fr>
2021-12-06 18:15:17 +0100

    debug form Trick

--- public/js/new_tricks.js
+++ public/js/new_tricks.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
     // Get the ul that holds the collection of tags
-   var $collectionHolder = $('ul.pictures','ul.videos');
 
     $('.add-item').on('click', function(e) {
         // prevent the link from creating a "#" on the URL
         e.preventDefault();
+        let $this = $(this);
+        let type = $this.data('type');
+        $collectionHolder = $('ul.' + type);
         
         // add a new tag form (see code block below)
-        addTagForm($collectionHolder, $(this));
+        addTagForm($collectionHolder, $this);
     });
     
     $(document).on('click', '.remove-tag', function(e) {
