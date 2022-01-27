c027b5c410ee6c6f14b92be240098a3552da4345
Marie <Marue59@hotmail.fr>
2022-01-27 14:37:25 +0100

    pagination

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -20,7 +20,10 @@ class CommentController extends AbstractController
     /**
      * @Route("/new/{id}", name="comment_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, EntityManagerInterface $entityManager, Trick $trick): Response
+    public function new(Request $request, 
+    EntityManagerInterface $entityManager, 
+    Trick $trick,
+    CommentRepository $commentRepository): Response
     {
         $comment = new Comment();
         //action pour rajouter l'url sur le form
@@ -36,6 +39,22 @@ class CommentController extends AbstractController
             $entityManager->flush();
 
             return $this->redirectToRoute('trick_show', ['slug'=>$trick->getSlug()], Response::HTTP_SEE_OTHER);
+        }
+
+        //pagination
+        $page = $request->get('page', 1);
+
+        $limit = 10;
+        $comments = $commentRepository->createQueryBuilder('t')
+            ->setFirstResult(($page * $limit) - $limit)
+            ->setMaxResults($limit)
+            ->getQuery()->getResult();
+
+        if ($request->isXmlHttpRequest()) {
+            return $this->render(
+                'comment/_list.html.twig',
+                ['comments' => $comments]
+            );
         }
 
         return $this->renderForm('comment/_form.html.twig', [
